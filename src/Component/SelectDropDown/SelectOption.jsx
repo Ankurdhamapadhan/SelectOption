9f57df1ebd3368349cc44d3fbfd6dc3e7f82acd5
Ankur Dhama <ankurdhamapadhan@gmail.com>
2024-09-10 10:45:51 +0530

    first commit

---
+++ src/Component/SelectDropDown/SelectOption.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SelectOption = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my custom React component!</p>
+    </div>
+  );
+};
+
+export default SelectOption;
